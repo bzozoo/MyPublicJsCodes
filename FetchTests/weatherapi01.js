03041202d27f0ebaf815b5e9b9d1151cdad7979f
Bata Zoltán <47896536+bzozoo@users.noreply.github.com>
2022-05-29 17:22:18 +0200

    Update weatherapi01.js

--- FetchTests/weatherapi01.js
+++ FetchTests/weatherapi01.js
@@ -1,26 +1,38 @@
-const APP_CONTAINER = document.querySelector('#app-weather')
-const API_ENDPOINT = "https://api.weatherapi.com/v1/current.json?key=86c37c482ca644efbdc142517222905&q=Budapest&aqi=no";
+const APP_CONTAINER = document.querySelector("#app-weather");
+const API_ENDPOINT =
+	"https://api.weatherapi.com/v1/current.json?key=86c37c482ca644efbdc142517222905&q=Budapest&aqi=no";
 
-async function getWeather(){
+async function getWeather() {
 	const response = await fetch(API_ENDPOINT);
 	const data = response.json();
 	return data;
 }
 
-function Template({location, current}){
+function Template({ location, current }) {
 	return `
 		<div class="widget-container">
 			<p>${location.name}</p>
 			<img src="${current.condition.icon}" />
 			<p>${current.temp_c} CELSIUS</p>
 		</div>
-	`
+	`;
 }
 
-async function init(){
-	const data = await getWeather();
-	console.log(APP_CONTAINER);
-	APP_CONTAINER.innerHTML = Template(data);
+function ErrorTemplate() {
+	return `
+	 The Backend server unavailable TRY AGAIN LATER...
+	`;
 }
 
-init()
+async function init() {
+	try {
+		const data = await getWeather();
+		APP_CONTAINER.innerHTML = Template(data);
+	} catch (e) {
+		console.error("Server error");
+		console.error(e);
+		APP_CONTAINER.innerHTML = ErrorTemplate();
+	}
+}
+
+init();
